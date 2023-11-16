e4f650db64bba76b7f3c16e9bde78fe200ed0cc1
vesrozeno <vesrozeno@gmail.com>
2023-11-16 10:14:55 -0300

    rm files and updates

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ bison -d yacc.y
 gcc -c lex.yy.c -o lex.yy.o
 gcc -c yacc.tab.c -o yacc.tab.o
 gcc -o parser lex.yy.o yacc.tab.o -lfl -lm
-./parser teste.txt
+./parser [arquivoteste.c]
 ```
 
 
--- teste.c
+++ teste.c
@@ -4,10 +4,10 @@ int main()
     abc = 24 + 32;
     if (!(abc > 10) && b == 4 + (6 % 2 != 4))
     {
-        // int a = hahaha(2);
+        // int a = b;
         a = (a + b + 2) * 2 / 3;
     }
-    else if (hahaha != hihihi)
+    else if (c != d)
     {
         int b, c, d, e, f;
         int a = 2, b = 4, c = a;
